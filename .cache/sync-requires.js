3cdc9cee52d42dd1ccb68fa61fc04577a331dfbe
Haris Aqeel <harisaqeel.2001@gmail.com>
2020-11-13 12:03:05 +0500

    Added

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -6,6 +6,7 @@ const preferDefault = m => (m && m.default) || m
 
 exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\umer pc\\Desktop\\Haris\\React Projects\\project-12b-crud-app\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\umer pc\\Desktop\\Haris\\React Projects\\project-12b-crud-app\\src\\pages\\404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\umer pc\\Desktop\\Haris\\React Projects\\project-12b-crud-app\\src\\pages\\index.js")))
 }
 
--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,6 @@
  */
 
 module.exports = {
-    /* Your site config here */
-    plugins: [`gatsby-plugin-material-ui`],
-  }
+  /* Your site config here */
+  plugins: [`gatsby-plugin-material-ui`],
+}
