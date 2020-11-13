f6fbae1379941c1b0eaa2c918c9e0a3afd616138
Haris Aqeel <harisaqeel.2001@gmail.com>
2020-11-13 07:41:01 +0500

    Added Gatsby

---
+++ .cache/sync-requires.js
@@ -0,0 +1,11 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\umer pc\\Desktop\\Haris\\React Projects\\project-12b-crud-app\\.cache\\dev-404-page.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\umer pc\\Desktop\\Haris\\React Projects\\project-12b-crud-app\\src\\pages\\index.js")))
+}
+
